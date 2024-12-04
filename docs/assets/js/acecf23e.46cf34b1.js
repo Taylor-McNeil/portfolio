226a6a94adb994ef16c029802bb3a756ba8f9da3
Taylor-McNeil <mcneiltaylor@live.com>
2024-12-03 23:55:45 -0500

    Bug Fix 2

---
+++ docs/assets/js/acecf23e.46cf34b1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[1903],{1912:o=>{o.exports=JSON.parse('{"blogBasePath":"/portfolio/blog","blogTitle":"Blog","authorsListPath":"/portfolio/blog/authors"}')}}]);
